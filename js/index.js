8c42d3c2d48e743e1e332094d2ac9bd5ca1b0e11
annyonok <annyonok@gmail.com>
2024-06-13 13:04:23 +0300

    add frontpage and menu

---
+++ js/index.js
@@ -0,0 +1,33 @@
+new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.portfolio__arrow_right',
+        prevEl: '.portfolio__arrow_left',
+        disabledClass: 'portfolio__arrow_disable',
+      },
+      // Responsive breakpoints
+    breakpoints: {
+    // when window width is >= 480px
+    480: {
+      grid: {
+        fill: 'row',
+        rows: 1
+      }
+    },
+    // when window width is >= 768px
+    768: {
+        slidesPerView: 2,
+        grid: {
+            fill: 'row',
+            rows: 2
+          }        
+        },
+    // when window width is >= 1440px
+    1440: {
+        slidesPerView: 2,
+        grid: {
+            fill: 'row',
+            rows: 2
+          }        
+        }
+  }
+});
